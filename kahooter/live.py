577b217f795965ac2f39b3d4e159161fa7e31027
Rohan Mathew <robozmat@gmail.com>
2021-07-12 14:55:55 +0100

    init

--- kahooter/live.py
+++ kahooter/live.py
@@ -3,213 +3,52 @@ import asyncio as asio
 import base64 as b64
 from urllib.parse import quote
 
-import websockets as wss
-
 from __init__ import *
 
 
-def run(pin, name, delay=.0):
-    # request challenge
-    c_rq = sess.get(f"https://kahoot.it/reserve/session/{pin}/?{t()}")
-    if "x-kahoot-session-token" not in c_rq.headers.keys():
-        print(f"Invalid code {pin}")
-        return
-    c = json.loads(c_rq.content)
+class Kahoot:
 
-    # get name
-    if c["namerator"] or name == "namerator":
-        name = namerator()
-    name = name.replace(" ", "")
-    print("Using name: " + name)
+    def __init__(self, pin: str, name: str, delay: float):
+        self.pin = pin
+        while not self.pin.isdigit():
+            self.pin = input("Pin: ")
+        self.name = name
+        if self.name == "":
+            self.name = input("Name (namerator): ")
+            self.name = "namerator" if self.name == "" else self.name
+        self.name = name.replace(" ", "")
+        self.delay = delay
+        self.sess_id = None
+        self.loop = asio.get_event_loop()
+        self.sess = rq.session()
 
-    ws_url = f"wss://kahoot.it/cometd/{pin}/{decrypt_websock(c['challenge'], c_rq.headers['x-kahoot-session-token'])}"
-    asio.get_event_loop().run_until_complete(live_async(ws_url, pin, name, delay))
+        # reserve place
+        rsp = self.sess.get(f"https://kahoot.it/reserve/session/{pin}/?{t()}")
+        if "x-kahoot-session-token" not in rsp.headers.keys():
+            print(f"Invalid code {pin}")
+            return
+        sess_token = rsp.headers["x-kahoot-session-token"]
 
+        c = json.loads(rsp.content)
+        challenge = c["challenge"]
 
-ans = []
+        self.sess_id = decrypt_sess(challenge, sess_token)
 
+        # get name
+        if c["namerator"] or name == "namerator":
+            self.name = namerator()
+        print("Using name: " + self.name)
 
-async def live_async(url, pin, name, delay):
-    device = rand_device()
-    logged_in = False
-    latest_id = 0
-    questions_started = False
-    global ans
+    def play(self):
+        # don't play if uninitialised
+        if not self.sess_id:
+            print("Uninitialized")
 
-    async with wss.connect(url) as ws:
-
-        # handshake + connect
-        await send(ws, [{
-            "id": "1",
-            "version": "1.0",
-            "minimumVersion": "1.0",
-            "channel": "/meta/handshake",
-            "supportedConnectionTypes": ["websocket", "long-polling", "callback-polling"],
-            "advice": {
-                "timeout": 60000,
-                "interval": 0,
-            },
-            "ext": {
-                "ack": True,
-                "timesync": {"tc": t(), "l": 0, "o": 0}
-            }
-        }])
-
-        rsp = await recv(ws)
-        cli_id = rsp[0]["clientId"]
-        await send(ws, [{
-            "id": str(int(rsp[0]["id"]) + 1),
-            "channel": "/meta/connect",
-            "connectionType": "websocket",
-            "advice": {"timeout": 0},
-            "clientId": cli_id,
-            "ext": {
-                "ack": 0,
-                "timesync": {"tc": t(), "l": 106, "o": 129}
-            }
-        }])
-
-        # start dance with router
-        dance = True
-        while dance:
-            rsp = await recv(ws)
-
-            for rsp in rsp:
-                if rsp["channel"] == "/service/controller":
-                    if "data" in rsp.keys():
-                        if rsp["data"]["type"] == "loginResponse":
-                            if "cid" not in rsp["data"].keys():
-                                print(f"Invalid code {pin}")
-                                return
-
-                elif rsp["channel"] == "/service/player":
-                    msg = json.loads(rsp["data"]["content"])
-
-                    if questions_started:
-                        if "timeLeft" in msg.keys() and "getReadyTimeRemaining" not in msg.keys():
-                            if msg["timeLeft"] > 0:
-                                # answer
-                                latest_id += 1
-                                if type(ans[msg["questionIndex"]]) == dict:
-                                    # single answer
-                                    post_ans = ans[msg["questionIndex"]]["idx"]
-                                    display_ans = ans[msg["questionIndex"]]["answer"]
-
-                                elif type(ans[msg["questionIndex"]]) == list:
-                                    # multiple answers
-                                    post_ans = [a["idx"] for a in ans[msg["questionIndex"]]]
-                                    display_ans = " | ".join([a["answer"] for a in ans[msg["questionIndex"]]])
-
-                                req = [{
-                                    "id": str(latest_id),
-                                    "channel": "/service/controller",
-                                    "data": {
-                                        "id": 45,
-                                        "type": "message",
-                                        "gameid": str(pin),
-                                        "host": "kahoot.it",
-                                        "content": {
-                                            "type": msg["gameBlockType"],
-                                            "choice": post_ans,
-                                            "questionIndex": msg["questionIndex"],
-                                            "meta": {"lag": 127}
-                                        }
-                                    },
-                                    "clientId": cli_id,
-                                    "ext": {}
-                                }]
-
-                                # json dumps content and send
-                                req[0]["data"]["content"] = json.dumps(req[0]["data"]["content"])
-                                time.sleep(.25 + delay)
-                                await send(ws, req)
-                                print(f"Q{msg['questionIndex'] + 1}: {display_ans}")
-
-                        elif "correctCount" in msg.keys():
-                            # quiz finished
-                            # disconnect
-                            await send(ws, [{
-                                "id": str(latest_id + 1),
-                                "channel": "/meta/disconnect",
-                                "clientId": cli_id,
-                                "ext": {
-                                    "timesync": {"tc": t(), "l": 127, "o": 2196}
-                                }
-                            }])
-
-                            # print summary
-                            print(f"""\n\nCompleted Quiz
-Player: {name}
- - Rank: {msg['rank']}
- - Score: {msg['totalScore']}
- - Correct: {msg['correctCount']} | Incorrect: {msg['incorrectCount']}""")
-                            dance = False
-
-                    else:
-                        # get answers
-                        if "quizTitle" in msg.keys():
-                            ans = find_answers(msg)
-
-                            if len(ans) == 0:
-                                print("Unable to find quiz answers, could be private")
-                                return
-
-                        if "playerV2" in rsp["data"]["content"]:
-                            await send(ws, [{
-                                "id": str(latest_id + 1),
-                                "channel": "/service/controller",
-                                "data": {
-                                    "id": rsp["data"]["id"] + 1,
-                                    "type": "message",
-                                    "gameid": pin,
-                                    "host": "kahoot.it",
-                                    "content": ""
-                                },
-                                "clientId": cli_id,
-                                "ext": {}
-                            }])
-                            time.sleep(0.7)
-
-                        elif "quizTitle" in rsp["data"]["content"]:
-                            questions_started = True  # quiz admin has started the quiz
-                            print("Quiz commenced")
-
-                elif rsp["channel"] == "/meta/connect":
-                    # acknowledge client is alive
-                    latest_id = int(rsp["id"]) + 1
-                    await send(ws, [{
-                        "id": str(latest_id),
-                        "channel": "/meta/connect",
-                        "connectionType": "websocket",
-                        "clientId": cli_id,
-                        "ext": {
-                            "ack": rsp["ext"]["ack"],
-                            "timesync": {"tc": t(), "l": 100, "o": 2260}
-                        }
-                    }])
-
-            # start quiz logic
-            if not logged_in:
-                time.sleep(1)
-                # join with player name
-                await send(ws, [{
-                    "id": str(latest_id + 1),
-                    "channel": "/service/controller",
-                    "data": {
-                        "type": "login",
-                        "gameid": str(pin),
-                        "host": "kahoot.it",
-                        "name": name,
-                        "content": json.dumps(device)
-                    },
-                    "clientId": cli_id,
-                    "ext": {}
-                }])
-                time.sleep(1)
-                logged_in = True
+        # url for websocket
+        ws_url = f"wss://kahoot.it/cometd/{self.pin}/{self.sess_id}"
 
 
-def decrypt_websock(js_key, sess_tok) -> str:
+def decrypt_sess(js_key, sess_tok) -> str:
     # decrypt cometd path
 
     # extract message and offset
@@ -225,7 +64,7 @@ def decrypt_websock(js_key, sess_tok) -> str:
         msg += chr((ord(c) * i + eval(offset_equation)) % 77 + 48)
 
     # base64 decode session token
-    b64_sess_tok = b64.decodebytes(bytes(sess_tok, "utf-8")).decode("utf-8")
+    b64_sess_tok = b64.b64decode(sess_tok).decode("utf-8", "strict")
 
     # xor message and base64 session token
     cometd_path = ""
@@ -239,6 +78,7 @@ def find_answers(details) -> list:
     cursor = 0
     answers = []
 
+    # loop through all results
     while True:
         quizzes = sess.get(
             f"https://create.kahoot.it/rest/kahoots/?query={quote(details['quizTitle'])}&limit=100&cursor={cursor}")
@@ -283,21 +123,65 @@ def find_answers(details) -> list:
     return answers
 
 
+codes = {
+    1: "GET_READY",
+    2: "START_QUESTION",
+    3: "GAME_OVER",
+    4: "TIME_UP",
+    5: "PLAY_AGAIN",
+    6: "ANSWER_SELECTED",
+    7: "ANSWER_RESPONSE",
+    8: "REVEAL_ANSWER",
+    9: "START_QUIZ",
+    10: "RESET_CONTROLLER",
+    11: "SUBMIT_FEEDBACK",
+    12: "FEEDBACK",
+    13: "REVEAL_RANKING",
+    14: "USERNAME_ACCEPTED",
+    15: "USERNAME_REJECTED",
+    16: "REQUEST_RECOVERY_DATA_FROM_PLAYER",
+    17: "SEND_RECOVERY_DATA_TO_CONTROLLER",
+    18: "JOIN_TEAM_MEMBERS",
+    19: "JOIN_TEAM_MEMBERS_RESPONSE",
+    20: "START_TEAM_TALK",
+    21: "SKIP_TEAM_TALK",
+    31: "IFRAME_CONTROLLER_EVENT",
+    32: "SERVER_IFRAME_EVENT",
+    40: "STORY_BLOCK_GET_READY",
+    41: "REACTION_SELECTED",
+    42: "REACTION_RESPONSE",
+    43: "GAME_BLOCK_START",
+    44: "GAME_BLOCK_END",
+    45: "GAME_BLOCK_ANSWER",
+    50: "SUBMIT_TWO_FACTOR",
+    51: "TWO_FACTOR_AUTH_INCORRECT",
+    52: "TWO_FACTOR_AUTH_CORRECT",
+    53: "RESET_TWO_FACTOR_AUTH"
+}
+
+
+def lookup(code) -> str:
+    if code in codes.keys():
+        return codes[code]
+    return ""
+
+
+def arg_start():
+    parser = ap.ArgumentParser()
+    parser.add_argument("-pin", "--pin", help="Pin of the quiz you are automating")
+    parser.add_argument("-name", "--name", default="namerator",
+                        help="Player name to use with the quiz (use \"namerator\" to use Kahoot's naming system)")
+    parser.add_argument("-d", "--ans_delay", default="0", help="(optional) Delay before answering question")
+    args = parser.parse_args()
+    try:
+        _ = args.pin
+    except AttributeError:
+        print("No \"code\" attribute passed")
+
+    k = Kahoot(args.pin, args.name, args.ans_delay)
+    k.play()
+
+
 if __name__ == "__main__":
-    def arg_start():
-        parser = ap.ArgumentParser()
-        parser.add_argument("-pin", "--pin", help="Pin of the quiz you are automating")
-        parser.add_argument("-name", "--name", default="namerator",
-                            help="Character name to use with the quiz (use \"namerator\" to use Kahoot's naming system)")
-        parser.add_argument("-d", "--ans_delay", default="0", help="(optional) Delay before answering question")
-        args = parser.parse_args()
-        try:
-            _ = args.pin
-        except AttributeError:
-            print("No \"code\" attribute passed")
-
-        run(args.pin, args.name, float(args.ans_delay))
-
-
     # arg_start()
     run("137653", "namerator", 0)
